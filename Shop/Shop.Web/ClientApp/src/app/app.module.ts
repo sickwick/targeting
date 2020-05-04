3895ec042518a4307a7cac96786791d8cddb1a1d
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-05-04 20:27:48 +0300

    Create board module with components

--- Shop/Shop.Web/ClientApp/src/app/app.module.ts
+++ Shop/Shop.Web/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import {BoardModule} from "./board/board.module";
+import {ItemBoardComponent} from "./board/components/item-board/item-board.component";
+import {ProductComponent} from "./board/components/product/product.component";
 
 @NgModule({
   declarations: [
@@ -16,16 +19,18 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    BoardModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'board', component: ItemBoardComponent },
     ])
   ],
   providers: [],
