a71dad398e48f4d09c9a3bff9fdec68fa7adc3f3
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-05-10 21:56:35 +0300

    New project for api & some fixes

---
+++ Shop/Shop.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,72 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Database.Models;
+using Shop.WebAPI.Interfaces;
+
+namespace Shop.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController: ControllerBase
+    {
+        private readonly IProductService _productService;
+        
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            try
+            {
+                return Ok(_productService.GetAllProducts());
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("product")]
+        public IActionResult GetProduct([FromQuery] Product product)
+        {
+            try
+            {
+                return Ok(_productService.GetProduct(product.Article));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("size")]
+        public IActionResult GetSizes([FromQuery] Product product)
+        {
+            try
+            {
+                return Ok(_productService.GetSizes(product.Article));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            try
+            {
+                return Ok(_productService.AddNewProduct(product));
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
