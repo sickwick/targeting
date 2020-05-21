06f159a123bd9dc146736c2efc3cd52eb827e3b4
Mihail Shcherbakov <mihail.shch@gmail.com>
2020-05-21 19:51:13 +0300

    Some steps to add product page

--- shop-client/src/app/product/product.module.ts
+++ shop-client/src/app/product/product.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfoComponent } from './components/info/info.component';
+import {SharedModule} from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [InfoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [InfoComponent]
 })
